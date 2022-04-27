88c8b3a513bf0030ee68e4cab9e528e4d22a40f4
Jaqueline Bianco <97712990+jaquezux@users.noreply.github.com>
2022-04-27 14:41:22 +0100

    Print years with World Cup

---
+++ 08_years-world-cup.py
@@ -0,0 +1,11 @@
+year = 1930
+
+for year in range(year, 2026, 4):
+    if (year == 1942 or year == 1946): #World Cup didn`t happened in these years due to the WW2.
+        continue
+    print(year)
+
+
+
+
+
